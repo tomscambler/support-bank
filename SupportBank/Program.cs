25f6071f4661f7df6bea86db3ddb87462dec7462
tomscambler <tomscambler@gmail.com>
2022-09-14 15:49:43 +0100

    SB-00: Added some files

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oh, hai! (^-^)");
+        }       
+    }
+
+}
